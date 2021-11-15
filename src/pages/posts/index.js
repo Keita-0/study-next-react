9bbd9601ac7aad5e4ea9607cc1d2fdd2b2719ccb
Keita-0 <laughlaughkaeru@gmail.com>
2021-11-15 23:39:50 +0900

    リファクタリング

--- src/pages/posts/index.js
+++ src/pages/posts/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
-import { Posts } from "../components/Posts";
-import styles from "../styles/Home.module.css";
+import { Posts } from "../../components/Posts";
+import styles from "../../styles/Home.module.css";
 
 const Home = () => {
   return (
